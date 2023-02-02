0eb49d5d33cbcc0405969a5016f6caa375301745
Sterling Baird <sterling.baird@icloud.com>
2023-02-01 20:38:25 -0700

    update get_matbench_data.py

--- training/get_matbench_data.py
+++ training/get_matbench_data.py
@@ -12,10 +12,6 @@ tasks_structure = {
     "matbench_mp_e_form": "e_form",
 }
 
-# tasks_structure = {'matbench_jdft2d': 'exfoliation_en', 'matbench_phonons': 'last phdos peak', 'matbench_dielectric': 'n', 'matbench_log_gvrh': 'log10(G_VRH)',
-#          'matbench_log_kvrh': 'log10(K_VRH)', 'matbench_perovskites': 'e_form', 'matbench_mp_gap': 'gap pbe', 'matbench_mp_is_metal': 'is_metal',
-#          'matbench_mp_e_form': 'e_form'}
-
 for index, (key, value) in enumerate(tqdm(tasks_structure.items())):
     df = load_dataset(key, pbar=True)
 
@@ -52,3 +48,11 @@ for index, (key, value) in enumerate(tasks_composition.items()):
     df = df.rename({value: "target"}, axis=1)
     df = df[["formula", "target"]]
     df.to_csv("matbench_datasets/{}.csv".format(key), index=False)
+
+1+1
+
+# %% Code Graveyard
+
+# tasks_structure = {'matbench_jdft2d': 'exfoliation_en', 'matbench_phonons': 'last phdos peak', 'matbench_dielectric': 'n', 'matbench_log_gvrh': 'log10(G_VRH)',
+#          'matbench_log_kvrh': 'log10(K_VRH)', 'matbench_perovskites': 'e_form', 'matbench_mp_gap': 'gap pbe', 'matbench_mp_is_metal': 'is_metal',
+#          'matbench_mp_e_form': 'e_form'}
