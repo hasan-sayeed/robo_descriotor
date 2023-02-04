5df3646459dcc46eaf8709445212f7abdd7a8d3b
sgbaird <sterling.baird@utah.edu>
2023-02-04 02:00:08 -0700

    Create compile_robofeat.py

---
+++ training/compile_robofeat.py
@@ -0,0 +1,31 @@
+import pickle
+import pymongo
+import json
+from tqdm import tqdm
+import pandas as pd
+
+collection_name = "matbench-datasets"
+database_name = "robocrystallographer"
+dataSource = "Cluster0"
+app_name = "data-cqjnk"
+cluster_uri = "cluster0.fyeompa"
+
+with open("training/secrets.json", "r") as f:
+    secrets = json.load(f)
+
+username = secrets["PYMONGO_USERNAME"]
+password = secrets["PYMONGO_PASSWORD"]
+
+client = pymongo.MongoClient(
+    f"mongodb+srv://{username}:{password}@{cluster_uri}.mongodb.net/?retryWrites=true&w=majority"  # noqa: E501
+)
+db = client[database_name]
+collection = db[collection_name]
+
+posts = collection.find({"cif": {"$exists": True}})
+results = [post for post in tqdm(posts)]
+
+df = pd.DataFrame(results)
+df.to_csv("matbench_datasets/robofeat.csv")
+
+1 + 1
